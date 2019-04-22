1e23084525cf64d5f8983c00b8cc5115a761def9
Roger Lipscombe <roger@differentpla.net>
2019-04-22 12:11:49 +0100

    Attempt to use nom to parse squirrel bytecode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate nom;
+
+use std::fs;
+
+named!(
+    parse_bytecode_stream,
+    tag!([0xfa, 0xfa])
+);
+
+fn main() -> Result<(), std::io::Error> {
+    let args: Vec<_> = std::env::args().collect();
+    let path = &args[1];
+    let bytecode = fs::read(path)?;
+    println!("{:?}", parse_bytecode_stream(&bytecode));
+    Ok(())
 }
